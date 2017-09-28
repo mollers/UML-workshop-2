84c7dbfec582e710f86d88f4bed666bf0042d146
KungenB <eb222sv@student.lnu.se>
2017-09-28 12:14:45 +0200

    memb

--- BoatClub/src/boatClub/Member.java
+++ BoatClub/src/boatClub/Member.java
@@ -6,31 +6,45 @@ public class Member {
 	private String personalNR;
 	private int id;
 	private ArrayList<Boat> boats = new ArrayList<Boat>();
+	
 	public String getName() {
 		return name;
 	}
+	
 	public void setName(String name) {
 		this.name = name;
 	}
+	
 	public String getPersonalNR() {
 		return personalNR;
 	}
+	
 	public void setPersonalNR(String personalNR) {
 		this.personalNR = personalNR;
 	}
+	
 	public int getId() {
 		return id;
 	}
+	
 	public void setId(int id) {
 		this.id = id;
 	}
+	
 	public int getNumberOfBoats() {
 		return this.boats.size();
 	}
+	
 	public ArrayList<Boat> getBoats() {
 		return boats;
 	}
+	
 	public void addBoats(Boat boat) {
 		this.boats.add(boat);
 	}
+	
+	public void deleteBoats(int index){
+		this.boats.remove(index-1);
+	}
+	
 }
