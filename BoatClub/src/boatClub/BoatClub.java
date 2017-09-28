92d463e4f978c840751b0e6674397627d4ea8ef7
KungenB <eb222sv@student.lnu.se>
2017-09-28 14:40:20 +0200

    boatclub+view

--- BoatClub/src/boatClub/BoatClub.java
+++ BoatClub/src/boatClub/BoatClub.java
@@ -1,5 +1,48 @@
 package boatClub;
 
-public class BoatClub {
+import java.util.ArrayList;
 
+public class BoatClub {
+	private Regestry tmp1;
+	private Controller controll;
+	
+	private Arraylist<Member> members;
+	private String[] memberInfo;
+	
+	
+	public void init(){
+		Registry reg = new Registry();
+		members = reg.loadRegistry();
+	}
+	
+	public void addMemmber(String name, String peronalNR) {
+		Member memb = new Member
+	}
+	
+	public void cMemberList(){
+		
+	}
+	
+	public void vMemberList(){
+		
+	}
+	
+	public void deleteMember(int id){
+		
+	}
+	
+	public void viewMember(int id){
+		
+	}
+	
+	public void registerBoat(int lenght, Boat.boatType??? ){
+		
+	}
+	
+	public void deleteBoat(int id, int index){
+		
+	}
+	public void changeBoaT(int id, int index, int size, Boat.boatType??){
+		
+	}
 }
--- BoatClub/src/view/view.java
+++ BoatClub/src/view/view.java
@@ -1,5 +1,22 @@
 package view;
 
 public class view {
-
+	public view() { 
+		
+	}
+	public void menu(){
+		System.out.println(
+			"==Boatclub Menu== \n"+
+			"0: Create new member \n"+
+			"1: Compact memmber list\n"+
+			"2: Verbose memmber list\n"+
+			"3: Delete memmber\n"+
+			"4: Change memmber info\n"+
+			"5: View member\n"+
+			"6: Register boat\n"+
+			"7: Delete boat\n"+
+			"8: Change boat info\n"+
+			"type the number in consloe and endter with enter"
+				);
+	}
 }
