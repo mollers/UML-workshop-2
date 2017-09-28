bd62068819e4b607edbbc7e4902a5afca92432b0
KungenB <eb222sv@student.lnu.se>
2017-09-28 11:41:26 +0200

    member

--- BoatClub/src/boatClub/Member.java
+++ BoatClub/src/boatClub/Member.java
@@ -1,9 +1,36 @@
 package boatClub;
 
+import java.util.ArrayList;
 public class Member {
 	private String name;
 	private String personalNR;
 	private int id;
-	private int numberOfBoats;
-	private Boat[] boats;
+	private ArrayList<Boat> boats = new ArrayList<Boat>();
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPersonalNR() {
+		return personalNR;
+	}
+	public void setPersonalNR(String personalNR) {
+		this.personalNR = personalNR;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public int getNumberOfBoats() {
+		return this.boats.size();
+	}
+	public Boat[] getBoats() {
+		return boats;
+	}
+	public void addBoats(Boat boat) {
+		this.boats.add(boat);
+	}
 }
