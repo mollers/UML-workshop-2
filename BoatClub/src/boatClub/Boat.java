5ef42d376c25706d5f96b6259947aaa3d34b154e
EMIL-DATOR\Emil <emil.emanuel@hotmail.com>
2017-09-28 11:01:33 +0100

    added size, type and functions to them

--- BoatClub/src/boatClub/Boat.java
+++ BoatClub/src/boatClub/Boat.java
@@ -1,5 +1,22 @@
 package boatClub;
 
 public class Boat {
-
+	private int size;
+	private String type;
+	
+	public void setSize (int i){
+		size = i;
+	}
+	
+	public int getSize (){
+		return size;
+	}
+	
+	public void setType (String t){
+		type = t;
+	}
+	
+	public String getType (){
+		return type;
+	}
 }
